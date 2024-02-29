a4a49030e116b6971b2690c0b8629f0a3a382ab1
ImanZuberi <161757800+ImanZuberi@users.noreply.github.com>
2024-02-29 13:31:32 -0500

    Add files via upload

---
+++ Html/js.js
@@ -0,0 +1,65 @@
+//alert ("Hello World")
+var text = "This class of dragons are known to be tough. These rock-eating dragons weaponize their meals by melting the rocks in their stomachs and turning it into lava, which is then used as a lava blast attack. Although these dragons tend to have smaller wings" 
+
+var notext = ""
+console.log("Console Message Test")
+
+
+/*
+$(".menu-area").click(function() {
+
+    $(".menu-area").toggleClass("active")
+
+    if ($(".menu-area").hasClass("active")){
+        
+        if ($(".menu-area").hasClass("active")){
+            $("<div></div>")
+            .html(text)
+            .addClass("appear")
+            .appendTo("#everything")
+            
+    }     
+  
+    }
+    else {
+        $("<div></div>")
+            .removeClass("appear")
+            .addClass("disappear")
+          
+    }
+    //
+
+})
+
+
+
+/*var x=4;
+
+if (x = 4) {
+   do this
+} else {
+    then do this instead
+}
+
+var studentName = "Ray"; //variable for one object
+var allStudents = ["Ray", "Naveena", "Turba", "Warrick", "Yuna", "Beatrice", "Pricilla"]//array for multiple variables
+console.log(allStudents)
+//alert (studentName)
+
+
+var i = 0;
+while (i < allStudents.length){
+    console.log (allStudents[i])
+    i = i + 1;
+}
+var k = 0;
+while (k < allStudents.length){
+    console.log (allStudents[k])
+    $("<div></div>")
+    .addClass("student")
+    .html(allStudents[k])
+    .appendTo("#everything")
+
+    k = k + 1;
+}
+*/
